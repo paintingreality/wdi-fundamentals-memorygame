d401a49443101142fcddd72c23b72867cfde8d81
Paul Alcaraz <paulalcaraz@Pauls-MBP.socal.rr.com>
2017-03-14 15:19:06 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,15 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardOne == cardTwo) {
+	console.log('You found a match!');	
+} else {
+  console.log('Sorry, try again.');
+}
+if (cardThree == cardFour) {
+	console.log('You found a match!');	
+} else {
+  console.log('Sorry, try again.');
+}
